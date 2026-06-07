7fd96d809586fc7cd2a6569205f317d536a4947d
AJO <78938243+Qaksito@users.noreply.github.com>
2026-06-07 18:25:45 -0300

    Update function.js

--- function.js
+++ function.js
@@ -7,6 +7,7 @@ let datos = [];
 
 const btnTipo = document.getElementById("btn-tipo");
 const btnVar = document.getElementById("btn-var");
+const contenedor = document.getElementById("grilla");
 
 btnTipo.addEventListener("click", function() {
     let nuevoNumero = tipografias.length + 1;
@@ -21,3 +22,21 @@ btnVar.addEventListener("click", function() {
     variables.push("Variable " + nuevoNumero);
     console.log("Eje Y actualizado:", variables);
 });
+
+function UpdateGrid(){
+    for (let i = 0 ; i < (tipografias.length * variables.length) ; i++){
+        let newBox = document.createElement("div");
+        if (i < tipografias.length){
+            newBox.className = "box-tipo"
+            newbox.textContent = "C-T"
+        }
+        else if(i % (tipografias.length + 1) === 0){
+            newBox.className = "box-var"
+            newbox.textContent = "C-V"
+        }
+        else{
+            newBox.className = "box-dato"
+            newbox.textContent = "C-D"
+        }
+    }
+}
